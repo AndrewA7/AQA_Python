533105db3aac4fce5355fd0f2ae4b98c59ddfc6a
mac <andrew.dzhulay@gmail.com>
2023-01-29 13:37:00 +0200

    old files deleted

--- HW5.1.py
+++
@@ -1,7 +0,0 @@
-side = int(input("enter side "))
-
-triangle = side
-
-for i in range(triangle):
-    print(triangle * "*")
-    triangle -= 1
--- HW5.2.py
+++
@@ -1,7 +0,0 @@
-other_side = int(input("enter side "))
-
-other_triangle = 0
-
-for i in range(other_side):
-    other_triangle += 1
-    print(other_triangle * "*")
